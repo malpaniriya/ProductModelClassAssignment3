9c42801b81df9afa842fd5e3aec6876808b121db
malpaniriya <riyamalpani75@gmail.com>
2023-10-23 10:49:06 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductModelClassAssignment3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductModelClassAssignment3.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+
+        public string Name { get; set; }
+        [Required]
+
+        public int Price { get; set; }
+        [Required]
+
+        public int Cid { get; set; }
+        [Required]
+
+        public string? ImageUrl { get; set; }
+
+        public int IsActive { get; set; }
+    }
+}
